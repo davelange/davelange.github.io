fae856a2e6adf21c78e0a5347306e6d434ba5127
David Lange <lange@finiam.com>
2023-12-13 14:31:05 +0000

    use tina cms

---
+++ tina/config.ts
@@ -0,0 +1,107 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Blog Posts",
+        path: "src/content/blog",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "publishedAt",
+            label: "Published at",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "canonical",
+            label: "Canonical link",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "sideProject",
+        label: "Side Projects",
+        path: "src/content/side-projects",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "repo",
+            label: "Repo link",
+          },
+          {
+            type: "string",
+            name: "url",
+            label: "Link",
+          },
+          {
+            type: "number",
+            name: "order",
+            label: "Position",
+          },
+        ],
+      },
+    ],
+  },
+});
